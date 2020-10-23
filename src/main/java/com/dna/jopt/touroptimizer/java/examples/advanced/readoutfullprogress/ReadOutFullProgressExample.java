f7e76841dd1efb17c14a4cc7ace1cd7d4d407e36
jens.richter@dna-evolutions.com <jens.richter@dna-evolutions.com>
2020-10-23 10:20:29 +0200

    Add new example for rading out progress object

---
+++ src/main/java/com/dna/jopt/touroptimizer/java/examples/advanced/readoutfullprogress/ReadOutFullProgressExample.java
@@ -0,0 +1,266 @@
+package com.dna.jopt.touroptimizer.java.examples.advanced.readoutfullprogress;
+/*-
+ * #%L
+ * JOpt TourOptimizer Examples
+ * %%
+ * Copyright (C) 2017 - 2020 DNA Evolutions GmbH
+ * %%
+ * This file is subject to the terms and conditions defined in file 'src/main/resources/LICENSE.txt',
+ * which is part of this repository.
+ *
+ * If not, see <https://www.dna-evolutions.com/>.
+ * #L%
+ */
+import static java.time.Month.MAY;
+import static tec.units.ri.unit.MetricPrefix.KILO;
+import static tec.units.ri.unit.Units.METRE;
+
+import java.io.IOException;
+import java.time.Duration;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+
+import javax.measure.Quantity;
+import javax.measure.quantity.Length;
+
+import com.dna.jopt.framework.body.IOptimization;
+import com.dna.jopt.framework.body.Optimization;
+import com.dna.jopt.framework.exception.caught.InvalidLicenceException;
+import com.dna.jopt.framework.outcomewrapper.IOptimizationProgress;
+import com.dna.jopt.framework.outcomewrapper.IOptimizationResult;
+import com.dna.jopt.member.unit.hours.IWorkingHours;
+import com.dna.jopt.member.bucket.entity.IEntity;
+import com.dna.jopt.member.unit.hours.IOpeningHours;
+import com.dna.jopt.member.unit.hours.WorkingHours;
+import com.dna.jopt.member.unit.hours.OpeningHours;
+import com.dna.jopt.member.unit.node.geo.TimeWindowGeoNode;
+import com.dna.jopt.member.unit.resource.CapacityResource;
+import com.dna.jopt.touroptimizer.java.examples.ExampleLicenseHelper;
+import tec.units.ri.quantity.Quantities;
+
+/**
+ * Example on how to access progress information via static method <code>
+ * toProgressString(IOptimizationProgress p)</code>".
+ *
+ * @author jrich
+ * @version Oct 23, 2020
+ * @since Oct 23, 2020
+ */
+public class ReadOutFullProgressExample extends Optimization {
+
+  /**
+   * The main method.
+   *
+   * @param args the arguments
+   * @throws IOException Signals that an I/O exception has occurred.
+   * @throws InvalidLicenceException the invalid licence exception
+   * @throws InterruptedException the interrupted exception
+   * @throws ExecutionException the execution exception
+   */
+  public static void main(String[] args)
+      throws IOException, InvalidLicenceException, InterruptedException, ExecutionException {
+    new ReadOutFullProgressExample().example();
+  }
+
+  /**
+   * To string.
+   *
+   * @return the string
+   */
+  public String toString() {
+    return "Example on how to access progress information via static method toProgressString(IOptimizationProgress p).";
+  }
+
+  /**
+   * Example.
+   *
+   * @throws IOException Signals that an I/O exception has occurred.
+   * @throws InvalidLicenceException the invalid licence exception
+   * @throws InterruptedException the interrupted exception
+   * @throws ExecutionException the execution exception
+   */
+  public void example()
+      throws IOException, InvalidLicenceException, InterruptedException, ExecutionException {
+
+    // Set license via helper
+    ExampleLicenseHelper.setLicense(this);
+
+    // Properties!
+    this.setProperties();
+
+    this.addNodes();
+    this.addResources();
+
+    ReadOutFullProgressExample.attachToObservables(this);
+
+    CompletableFuture<IOptimizationResult> resultFuture = this.startRunAsync();
+
+    // It is important to block the call, otherwise optimization will be terminated
+    System.out.println(resultFuture.get());
+  }
+
+  /** Sets the properties. */
+  private void setProperties() {
+
+    Properties props = new Properties();
+
+    props.setProperty("JOptExitCondition.JOptGenerationCount", "2000");
+    props.setProperty("JOpt.Algorithm.PreOptimization.SA.NumIterations", "10000");
+    props.setProperty("JOpt.Algorithm.PreOptimization.SA.NumRepetions", "1");
+    props.setProperty("JOpt.NumCPUCores", "4");
+
+    this.addElement(props);
+  }
+
+  /** Adds the resources. */
+  private void addResources() {
+
+    List<IWorkingHours> workingHours = new ArrayList<>();
+    workingHours.add(
+        new WorkingHours(
+            ZonedDateTime.of(2020, MAY.getValue(), 6, 8, 0, 0, 0, ZoneId.of("Europe/Berlin")),
+            ZonedDateTime.of(2020, MAY.getValue(), 6, 18, 0, 0, 0, ZoneId.of("Europe/Berlin"))));
+
+    workingHours.add(
+        new WorkingHours(
+            ZonedDateTime.of(2020, MAY.getValue(), 7, 8, 0, 0, 0, ZoneId.of("Europe/Berlin")),
+            ZonedDateTime.of(2020, MAY.getValue(), 7, 18, 0, 0, 0, ZoneId.of("Europe/Berlin"))));
+
+    Duration maxWorkingTime = Duration.ofHours(10);
+    Quantity<Length> maxDistanceKmW = Quantities.getQuantity(1200.0, KILO(METRE));
+
+    CapacityResource rep1 =
+        new CapacityResource(
+            "Jack", 50.775346, 6.083887, maxWorkingTime, maxDistanceKmW, workingHours);
+    rep1.setCost(0, 1, 1);
+    this.addElement(rep1);
+  }
+
+  /** Adds the nodes. */
+  private void addNodes() {
+
+    List<IOpeningHours> weeklyOpeningHours = new ArrayList<>();
+    weeklyOpeningHours.add(
+        new OpeningHours(
+            ZonedDateTime.of(2020, MAY.getValue(), 6, 8, 0, 0, 0, ZoneId.of("Europe/Berlin")),
+            ZonedDateTime.of(2020, MAY.getValue(), 6, 12, 0, 0, 0, ZoneId.of("Europe/Berlin"))));
+
+    weeklyOpeningHours.add(
+        new OpeningHours(
+            ZonedDateTime.of(2020, MAY.getValue(), 7, 8, 0, 0, 0, ZoneId.of("Europe/Berlin")),
+            ZonedDateTime.of(2020, MAY.getValue(), 7, 12, 0, 0, 0, ZoneId.of("Europe/Berlin"))));
+
+    Duration visitDuration = Duration.ofMinutes(20);
+
+    // Define some nodes
+    TimeWindowGeoNode koeln =
+        new TimeWindowGeoNode("Koeln", 50.9333, 6.95, weeklyOpeningHours, visitDuration, 1);
+    this.addElement(koeln);
+
+    TimeWindowGeoNode essen =
+        new TimeWindowGeoNode("Essen", 51.45, 7.01667, weeklyOpeningHours, visitDuration, 1);
+    this.addElement(essen);
+
+    TimeWindowGeoNode dueren =
+        new TimeWindowGeoNode("Dueren", 50.8, 6.48333, weeklyOpeningHours, visitDuration, 1);
+    this.addElement(dueren);
+
+    TimeWindowGeoNode wuppertal =
+        new TimeWindowGeoNode("Wuppertal", 51.2667, 7.18333, weeklyOpeningHours, visitDuration, 1);
+    this.addElement(wuppertal);
+
+    TimeWindowGeoNode aachen =
+        new TimeWindowGeoNode("Aachen", 50.775346, 6.083887, weeklyOpeningHours, visitDuration, 1);
+    this.addElement(aachen);
+  }
+
+  /**
+   * To progress string.
+   *
+   * @param p the p
+   * @return the string
+   */
+  private static String toProgressString(IOptimizationProgress p) {
+
+    IEntity winner = p.getResultEntity();
+
+    StringBuilder progressSB = new StringBuilder();
+
+
+    long fullRouteTimeMillisExcludingFlexTime =
+        winner.getJoinedDetailController().getCurTransitTime()
+            + winner.getJoinedDetailController().getCurIdleTime()
+            + winner.getJoinedDetailController().getCurProductiveTime();
+
+    long fullRouteTimeHours = fullRouteTimeMillisExcludingFlexTime / 1000L / 3600L;
+
+    double utilization = 0.0;
+
+    if (fullRouteTimeMillisExcludingFlexTime > 0) {
+      utilization =
+          (double) (winner.getJoinedDetailController().getCurProductiveTime())
+              / ((double) fullRouteTimeMillisExcludingFlexTime);
+    }
+
+    progressSB.append("PC " + p.getProgress());
+    progressSB.append(", AL " + p.getCallerId());
+    progressSB.append(
+        ", JC "
+            + winner.getJoinedCost()
+            + ", RC "
+            + winner.getRouteCount()
+            + ", EC "
+            + winner.getTotalRoutesOptimizableElementsCount()
+            + ", TC "
+            + (winner.getRouteCount() + winner.getTotalRoutesOptimizableElementsCount()));
+    progressSB.append(", TT[h] " + ((int) fullRouteTimeHours)); // Total time
+    progressSB.append(", TU[%] " + (int) (utilization * 100.0)); // Total utilization
+    progressSB.append(
+        ", TD[km] "
+            + ((int) winner.getJoinedDetailController().getCurDistance()
+                / 1000.0)); // Total distance
+
+    return progressSB.toString();
+  }
+
+  /**
+   * Attach to observables.
+   *
+   * @param opti the opti
+   */
+  private static void attachToObservables(IOptimization opti) {
+
+    opti.getOptimizationEvents()
+        .progressSubject()
+        .subscribe(
+            p -> {
+              System.out.println(toProgressString(p));
+            });
+
+    opti.getOptimizationEvents()
+        .warningSubject()
+        .subscribe(
+            w -> {
+              System.out.println(w.toString());
+            });
+
+    opti.getOptimizationEvents()
+        .statusSubject()
+        .subscribe(
+            s -> {
+              System.out.println(s.toString());
+            });
+
+    opti.getOptimizationEvents()
+        .errorSubject()
+        .subscribe(
+            e -> {
+              System.out.println(e.toString());
+            });
+  }
+}
