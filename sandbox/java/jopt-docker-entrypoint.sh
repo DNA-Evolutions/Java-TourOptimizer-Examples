6ae838e1eb40b52e9ed126d34932af5fc50f3047
jens.richter@dna-evolutions.com <jens.richter@dna-evolutions.com>
2021-05-26 16:04:38 +0200

    Add java sandbox

---
+++ sandbox/java/jopt-docker-entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+set -eu
+
+echo "Trying to download Jopt Examples"
+
+folder="/home/coder/project/jopt.examples/"
+url="https://github.com/DNA-Evolutions/Java-TourOptimizer-Examples.git"
+if ! git clone "${url}" "${folder}" 2>/dev/null && [ -d "${folder}" ] ; then
+    echo "Clone skipped as the folder ${folder} already exists. If you want a 'fresh' clone, choose another volume or rename the existing folder."
+fi
+
+exec /usr/bin/entrypoint.sh "$@"
