dfc32f49862b2338242c395bf8e1dec6e7fabfcb
jensrichter <jensrichter24@googlemail.com>
2025-02-11 18:31:25 +0100

    Update license to allow 20 elements

--- src/main/java/com/dna/jopt/touroptimizer/java/examples/ExampleLicenseHelper.java
+++ src/main/java/com/dna/jopt/touroptimizer/java/examples/ExampleLicenseHelper.java
@@ -19,19 +19,25 @@ import com.dna.jopt.framework.body.IOptimization;
 
 public class ExampleLicenseHelper {
 
-    public static final String PUBLIC_JSON_LICENSE = "{\r\n" + "	\"version\": \"1.1\",\r\n"
-	    + "	\"identifier\": \"PUBLIC-\",\r\n"
-	    + "	\"description\": \"Key provided to for evaluation purpose from DNA evolutions GmbH.\",\r\n"
-	    + "	\"contact\": \"www.dna-evolutions.com\",\r\n" + "	\"modules\": [{\r\n"
-	    + "			\"Module:\": \"Elements\",\r\n" + "			\"max\": 15\r\n"
-	    + "		}, {\r\n" + "			\"Module:\": \"Date\",\r\n"
-	    + "			\"creation\": \"2021-05-25\",\r\n" + "			\"due\": \"2027-05-25\"\r\n"
-	    + "		}\r\n" + "	],\r\n"
-	    + "	\"key\": \"PUBLIC-bc799ef350fe9841c1354736d8f863cb85bac88cefd19960c1\"\r\n" + "}";
+    public static final String PUBLIC_JSON_LICENSE = "{\r\n"
+    	+ "  \"version\" : \"1.2\",\r\n"
+    	+ "  \"identifier\" : \"PUBLIC-\",\r\n"
+    	+ "  \"description\" : \"Key provided to for evaluation purpose from DNA evolutions GmbH.\",\r\n"
+    	+ "  \"contact\" : \"www.dna-evolutions.com\",\r\n"
+    	+ "  \"modules\" : [ {\r\n"
+    	+ "    \"Module:\" : \"Elements\",\r\n"
+    	+ "    \"max\" : 20\r\n"
+    	+ "  }, {\r\n"
+    	+ "    \"Module:\" : \"Date\",\r\n"
+    	+ "    \"creation\" : \"2025-02-04\",\r\n"
+    	+ "    \"due\" : \"2029-01-28\"\r\n"
+    	+ "  } ],\r\n"
+    	+ "  \"key\" : \"PUBLIC-e6dc49fcbda599f45638d39794fd4f99b062c2ae96864e37ef\"\r\n"
+    	+ "}";
 
     private static final String LICENSE_PATH = "src/main/resources/license/YOUR_LICENSE.jli";
 
-    private ExampleLicenseHelper() {
+    protected ExampleLicenseHelper() {
 	// Nothing to do
     }
 
